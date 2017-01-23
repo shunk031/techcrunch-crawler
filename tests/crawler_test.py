0b1d3f8f118c57f5e9ddc830202e0518fb07a615
Shunsuke KITADA <septemtrio.ager@gmaul.com>
2017-01-23 22:10:40 +0900

    update test script

--- tests/crawler_test.py
+++ tests/crawler_test.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import os
 sys.path.append(os.pardir)
@@ -10,23 +12,35 @@ HOME_DIR = os.path.expanduser("~")
 
 if __name__ == '__main__':
 
-    TECHCRUNCH_URL = "https://techcrunch.com/page/1"
-    crawler = TechcrunchCrawler(TECHCRUNCH_URL)
+    if os.path.isfile("status.json"):
+        with open("status.json", "r") as rf:
+            status_dict = json.load(rf)
+
+        TECHCRUNCH_URL = status_dict["target_url"]
+        page_count = status_dict["page_count"]
+
+    else:
+        TECHCRUNCH_URL = "https://techcrunch.com/page/186"
+        page_count = 186
+
+    crawler = TechcrunchCrawler(TECHCRUNCH_URL, "./dump_files", page_count=page_count)
+
+    slacker_config = os.path.join(HOME_DIR, ".slacker.config")
+    with open(slacker_config, "r") as rf:
+        config = json.load(rf)
+
+    slacker = Slacker(config["token"])
 
     try:
-        crawler.crawl()
+        finish_crawl = crawler.crawl()
+        slacker.chat.post_message("#crawler", "{}: {}".format(crawler.__class__.__name__, finish_crawl))
 
     except Exception as e:
-        slacker_config = os.path.join(HOME_DIR, ".slacker.config")
-        with open(slacker_config, "r") as rf:
-            config = json.load(rf)
-
-        slacker = Slacker(config["token"])
 
         trial = 0
         while trial < 3:
             try:
-                slacker.chat.post_message("#crawler", e)
+                slacker.chat.post_message("#crawler", "{}: {}".format(crawler.__class__.__name__, e))
             except Exception:
                 trial += 1
             else:
